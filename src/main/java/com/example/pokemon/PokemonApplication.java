acef7ac28f10ee54d40dbf5901ecbcfa1d740ae1
Alejandro Espiritu <alex32.taem@gmail.com>
2022-04-20 11:46:25 -0500

    make it better

--- src/main/java/com/example/pokemon/PokemonApplication.java
+++ src/main/java/com/example/pokemon/PokemonApplication.java
@@ -2,7 +2,7 @@ package com.example.pokemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//sda
 @SpringBootApplication
 public class PokemonApplication {
 
